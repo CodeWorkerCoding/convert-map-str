3e329d3ce37f333ff3a1d07748ec12cb1cd29c0a
master-alen <nchufujianjian@126.com>
2016-11-22 18:22:17 +0800

    create git repository

---
+++ src/main/java/com/edu/nchu/convert/App.java
@@ -0,0 +1,12 @@
+package com.edu.nchu.convert;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
